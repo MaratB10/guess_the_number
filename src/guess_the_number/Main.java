3f7bdc244dc669b1cbccbb9476886f1ce230696d
MaratB10 <bmt1986@yandex.ru>
2020-08-30 21:45:09 +0300

    new

--- src/guess_the_number/Main.java
+++ src/guess_the_number/Main.java
@@ -1,30 +1,42 @@
 package guess_the_number;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
-        System.out.println("Ваша задача угадать число: ");
-        int range = 100;
-        int number = (int)(Math.random() * range );
-        playLevel(range,number);
-        scanner.close();
+
+        System.out.println("Ваша задача угадать число ");
+
+// генерируем случайное число, которое будем угадывать
+
+        Random random = new Random();
+        int randNum = random.nextInt(9);
+
+        playLevel(randNum);
     }
 
-    private static void playLevel (int range, int number) {
-        while (true){
-            System.out.println("Угадайте число от 0 до " + range);
+    private static void playLevel (int number) {
+        System.out.println("У тебя 3 попытки, чтобы угадать число от 0 до 9");
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("------------------");
+            System.out.println("Попытка №" + i);
             int input_number = scanner.nextInt();
             if (input_number == number){
-                System.out.println("Вы угадали");
+                System.out.println("Вы угадали!!!");
                 break;
-            } else if (input_number > number){
+            } else if (input_number > number && i != 3){
                 System.out.println("Загаданное число меньше");
-            } else {
+            } else if (input_number < number && i != 3) {
                 System.out.println("Загаданное число больше");
+            } else {
+                System.out.println("Вы проиграли!!!");
             }
         }
     }
